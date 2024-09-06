75e94d5c65e3a29024d9ac1f682f088d94baa24a
maykonmarcosjuniordev-star <maykon.marcos.junior.dev@gmail.com>
2024-09-06 18:31:41 -0300

    corrigindo bugs e organizando os logs

--- src/comparator.py
+++ src/comparator.py
@@ -68,38 +68,20 @@ def compare(df1: pd.DataFrame, df2: pd.DataFrame
 
     return total_errors, line_errors, col_errors, errors_per_line
 
-def main():
-    # Lê arquivos
-    df1 = vf.format_data(SOURCE)
-    df2 = vf.format_data(TEST)
-    # df2 = pd.read_excel(TEST)
-    # df2, draws = md.get_average_sheet(df_temp)
-    #    if draws > 0:
-    #        print("Número de Empates:", draws)
-    
-    # Compara arquivos
-    (total_errors, line_errors, errors_per_col, errors_per_line) = compare(df1, df2)
-
-    if total_errors < 0:
-        return -1
-
-    n_sentences, n_variaveis = df1.shape
-    n_lines, n_variaveis = df2.shape
-    # Por causa da coluna com o id da sentença
-    n_variaveis -= 1
-    total_values = n_sentences * (n_variaveis)
-
+def print_results(total_errors, sentence_errors, errors_per_col,
+                  errors_per_line, n_sentences, n_variaveis, variaveis):
     print("Número de Sentenças:", n_sentences)
     print("Número de Variáveis:", n_variaveis)
+    total_values = n_sentences * n_variaveis
     print("Número Total de Valores:", total_values)
 
     # Imprime resultados
-    print('\nNúmero Total de Erros:', total_errors)
-    print("Porcentagem Total de Erros: %.2f%%" % (total_errors/total_values * 100))
-    print("\n---------------- Sentenças -----------------\n")
-    print('Número de Sentenças com Erros:', line_errors)
-    print("Porcentagem de Sentenças com Erros: %.2f%%" % (line_errors/n_lines * 100))
+    print('\nNúmero Total de Erros:', total_errors,
+          '-->', "%.2f%%" % (total_errors/total_values * 100))
+    print('Número de Sentenças com Erros:', sentence_errors,
+          '-->', "%.2f%%" % (sentence_errors/n_sentences * 100))
     if ERROS_POR_SENTENCA:
+        print("\n---------------- Sentenças -----------------\n")
         print('\nErros por Sentença:')
     media_de_erros_por_sentenca = 0
     maior_erro_em_uma_sentenca = 0
@@ -115,26 +97,26 @@ def main():
             dec_num = round(number - num_int, 2)*100
             resultado = 'erros, %.2d.%.2d%% de erro' % (number, dec_num)
             print(id, resultado)
-    media_de_erros_por_sentenca //= n_sentences
-    print('\nMaior Erro em uma Sentença:', maior_erro_em_uma_sentenca)
-    print('Média de Erros por Sentença:', media_de_erros_por_sentenca)
-    print("Porcentagem Média de Erros por Sentença: %.2f%%" % (media_de_erros_por_sentenca/(n_variaveis) * 100))
+    media_de_erros_por_sentenca /= n_sentences
+    print('Média de Erros por Sentença:', round(media_de_erros_por_sentenca, 2),
+          ('--> %.2f%%' % (media_de_erros_por_sentenca/(n_variaveis) * 100)))
+    print('Maior nº de Erros em uma Sentença:', maior_erro_em_uma_sentenca,
+          ('--> %.2f%%' % (maior_erro_em_uma_sentenca/(n_variaveis) * 100)))
     col_errors = 0
     for i in errors_per_col:
         col_errors += int(i > 0)
     print("\n--------------- Variáveis ----------------\n")
-    print('Número de Variáveis com Erros:', col_errors)
-    print("Porcentagem de Variáveis com Erros: %.2f%%" % (col_errors/n_variaveis * 100))
+    print('Número de Variáveis com Erros:', col_errors,
+          ('--> %.2f%%' % ((col_errors/n_variaveis) * 100)))
     print('\nErros por Variável:')
     media_de_erros_por_variavel = 0
     maior_erro_em_uma_variavel = 0
-    variaveis = df1.columns
     for i in range(1, n_variaveis + 1):
         id = (" %.2d" % i) + (' - %.3d' % errors_per_col[i])
         media_de_erros_por_variavel += errors_per_col[i]
         if errors_per_col[i] > maior_erro_em_uma_variavel:
             maior_erro_em_uma_variavel = errors_per_col[i]
-        number = (errors_per_col[i]/n_lines * 100)
+        number = (errors_per_col[i]/n_sentences * 100)
         num_int = int(number)
         dec_num = round(number - num_int, 2)*100
         if dec_num == 100:
@@ -143,9 +125,29 @@ def main():
         resultado = 'erros, %.2d.%.2d%% do total' % (number, dec_num)
         print(id, resultado, ':', variaveis[i])
     media_de_erros_por_variavel //= n_variaveis
-    print('\nMaior Número de Erros em uma Variável:', maior_erro_em_uma_variavel)
-    print('Média de Erros por Variável:', media_de_erros_por_variavel)
-    print("Porcentagem Média de Erros por Variável: %.2f%%" % (media_de_erros_por_variavel/n_lines * 100))
+    print('\nMédia de Erros por Variável:', media_de_erros_por_variavel,
+          ('--> %.2f%%' % ((media_de_erros_por_variavel/n_sentences) * 100)))
+    print('Maior Número de Erros em uma Variável:', maior_erro_em_uma_variavel,
+          ('--> %.2f%%' % ((maior_erro_em_uma_variavel/n_sentences) * 100)))
+    print("------------------------------------------->>")
+    print(">> Acurácia Total: %.2f%%" % (100 - ((total_errors/total_values) * 100)))
+
+def main():
+    # Lê arquivos
+    df1 = vf.format_data(SOURCE)
+    df2 = vf.format_data(TEST)
+    
+    # Compara arquivos
+    (total_errors, sentence_errors, errors_per_col, errors_per_line) = compare(df1, df2)
+
+    if total_errors < 0:
+        return -1
+    n_sentences, n_variaveis = df1.shape
+    # Por causa da coluna com o id da sentença
+    n_variaveis -= 1
+    variaveis = df1.columns
+    print_results(total_errors, sentence_errors, errors_per_col,
+                  errors_per_line, n_sentences, n_variaveis, variaveis)
     return 0
 
 if __name__ == '__main__':
--- src/parameters.py
+++ src/parameters.py
@@ -1,19 +1,43 @@
 # caminhos para os arquivos de dados e modelos
 ARQUIVO_DE_SAIDA = 'Resultados/resultado.xlsx'
-SOURCE = 'tables/source.xlsx'
-TEST = 'tables/test.xlsx'
+LOG_VAR = 'Resultados/log_variables.txt'
+SOURCE = 'tables/source.csv'
+TEST = 'tables/test.csv'
 
 
-USE_RANGES = True
 CREATE_RANGES = True
 ERROS_POR_SENTENCA = False
+
+FAIXAS_EXTRAVIO = [1, 24, 72, 168]
+FAIXAS_ATRASO = [1, 4, 8, 12, 16, 24, 28]
+FAIXAS_DANO = [1, 2000, 4000, 6000, 8000, 10000]
+
+# Variáveis contínuas
+DATA_VARS = [
+    "sentenca",
+    'direito_de_arrependimento',
+    'descumprimento_de_oferta',
+    'extravio_definitivo',
+    'extravio_temporario',
+    'intervalo_extravio_temporario',
+    'violacao_furto_avaria',
+    'cancelamento/alteracao_destino',
+    'atraso',
+    'intervalo_atraso',
+    'culpa_exclusiva_consumidor',
+    'condicoes_climaticas/fechamento_aeroporto',
+    'noshow',
+    'overbooking',
+    'assistencia_cia_aerea',
+    'hipervulneravel'
+]
 
 # Variáveis categorizadas
 DATA_VARS_RANGE = [
     'direito_de_arrependimento',
     'descumprimento_de_oferta',
     'extravio_definitivo',
-    'extravio_temporario',
+    'intervalo_extravio_temporario',
     'faixa_intervalo_extravio_temporario',
     'violacao_furto_avaria',
     'cancelamento/alteracao_destino',
@@ -26,84 +50,5 @@ DATA_VARS_RANGE = [
     'assistencia_cia_aerea',
     'hipervulneravel'
 ]
-FAIXAS_EXTRAVIO = [1, 24, 72, 168]
-FAIXAS_ATRASO = [1, 4, 8, 12, 16, 24, 28]
-FAIXAS_DANO = [1, 2000, 4000, 6000, 8000, 10000]
+# DATA_VARS = DATA_VARS_RANGE
 
-# Variáveis contínuas
-DATA_VARS_CONTINUOUS = [
-    "sentenca",
-    'direito_de_arrependimento',
-    'descumprimento_de_oferta',
-    'extravio_definitivo',
-    'extravio_temporario',
-    'intervalo_extravio_temporario',
-    'violacao_furto_avaria',
-    'cancelamento/alteracao_destino',
-    'atraso',
-    'intervalo_atraso',
-    'culpa_exclusiva_consumidor',
-    'condicoes_climaticas/fechamento_aeroporto',
-    'noshow',
-    'overbooking',
-    'assistencia_cia_aerea',
-    'hipervulneravel'
-]
-
-# DATA_VARS = DATA_VARS_RANGE if USE_RANGES else DATA_VARS_CONTINUOUS
-
-DATA_VARS = DATA_VARS_CONTINUOUS
-
-ATRASO2 = [
-    "sentenca",
-    'direito_de_arrependimento',
-    'descumprimento_de_oferta',
-    'extravio_definitivo',
-    'extravio_temporario',
-    'intervalo_extravio_temporario',
-    'violacao_furto_avaria',
-    'atraso',
-    'intervalo_atraso',
-    'culpa_exclusiva_consumidor',
-    'condicoes_climaticas/fechamento_aeroporto',
-    'noshow',
-    'overbooking',
-    'assistencia_cia_aerea',
-    'hipervulneravel'
-]
-ATRASO5 = [
-    "sentenca",
-    'direito_de_arrependimento',
-    'descumprimento_de_oferta',
-    'extravio_definitivo',
-    'extravio_temporario',
-    'intervalo_extravio_temporario',
-    'violacao_furto_avaria',
-    'intervalo_atraso',
-    'culpa_exclusiva_consumidor',
-    'condicoes_climaticas/fechamento_aeroporto',
-    'noshow',
-    'overbooking',
-    'assistencia_cia_aerea',
-    'hipervulneravel'
-]
-
-EXTRAVIO2 = [
-    "sentenca",
-    'direito_de_arrependimento',
-    'descumprimento_de_oferta',
-    'extravio_definitivo',
-    'intervalo_extravio_temporario',
-    'violacao_furto_avaria',
-    'cancelamento/alteracao_destino',
-    'atraso',
-    'intervalo_atraso',
-    'culpa_exclusiva_consumidor',
-    'condicoes_climaticas/fechamento_aeroporto',
-    'noshow',
-    'overbooking',
-    'assistencia_cia_aerea',
-    'hipervulneravel'
-]
-
-DATA_VARS = ATRASO5
